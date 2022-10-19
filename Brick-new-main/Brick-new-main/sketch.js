c23ef8722d23cffe9f15d31276531553c72391f7
Kristians12345 <100486066+Kristians12345@users.noreply.github.com>
2022-10-19 18:50:12 +0100

    Add files via upload

---
+++ Brick-new-main/Brick-new-main/sketch.js
@@ -0,0 +1,37 @@
+var brickImage , ballImage , paddleImage
+var paddle
+function preload() {
+  backgroundImage = loadImage("./assets/background.jpg");
+  brickImage = loadImage("assets/brick.png")
+  ballImage = loadImage("assets/ball.png")
+  paddleImage = loadImage("assets/paddle.png")
+}
+
+function setup() {
+  canvas = createCanvas(800, 600);
+
+ball = createSprite(400,300,20,20)
+ball.addImage("ball",ballImage)
+ball.scale = 0.2
+
+paddle = createSprite(400,500,20,20)
+paddle.addImage("paddle",paddleImage)
+paddle.scale = 0.5
+
+
+}
+ function draw(){
+  background(backgroundImage)
+
+  createEdgeSprites()
+  ball.bounceOff(paddle);
+  ball.bounceOff(topEdge);
+
+  paddle.x = mouseX
+  if(keyDown("space")){
+    ball.velocityX = 10;
+    ball.velocityY = 10;
+  }
+
+  drawSprites()
+ }
